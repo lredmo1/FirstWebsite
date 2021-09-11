183d9c953296c18ae4d62b05a235d6efcbdeee09
Lisa <redlisa125@gmail.com>
2021-09-10 16:11:37 -1000

    First phase complete minus JS

---
+++ script.js
@@ -0,0 +1,10 @@
+const input = document.getElementsByClassName('largeIcon');
+
+function hoverSize() {
+    if (img.height === 300px) {
+        img.height = 400px;
+        img.width = 400px;
+    }
+}
+
+largeIcon.addEventListener('mouseover', hoverSize)
